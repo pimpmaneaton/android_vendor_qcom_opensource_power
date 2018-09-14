81b6a1de09d13633972f016282cd1d083c5ea848
Vinay Verma <vvinay@codeaurora.org>
2018-09-14 11:25:46 +0530

    Reduce hwbinder buffer size for power HAL
    Set hwbinder buffer size to 16KB for 32 bit architecture.
    
    Change-Id: I4272a12853269dd6363f58a5bb81caa09cc47e9a
    CRs-Fixed: 2315283

--- Android.mk
+++ Android.mk
@@ -13,6 +13,7 @@ LOCAL_SHARED_LIBRARIES := \
     libbase \
     libhidlbase \
     libhidltransport \
+    libhwbinder \
     libutils \
     android.hardware.power@1.2
 
@@ -141,6 +142,10 @@ endif
 
 ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
     LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
+
+ifeq ($(TARGET_ARCH),arm)
+    LOCAL_CFLAGS += -DARCH_ARM_32
 endif
 
 ifneq ($(TARGET_USES_NON_LEGACY_POWERHAL), true)
